5d3d85c9c277b361b0f8c2f7d0bf6b73608f9de4
Matthieu <matthieuraymond@hotmail.com>
2015-05-19 16:52:40 +0200

    add table

--- src-2015/data/scripts/level.lua
+++ src-2015/data/scripts/level.lua
@@ -1,7 +1,10 @@
 
 -- main platform color
 platform = color(255,0,0)
-tile(platform, 'tiles.png', 0,0,32,32)
+tile(platform, 'vide.png', 0,0,32,32)
+
+-- Table
+tile(color(0,0,255), 'table.png', 0,0, 182,126)
 
 -- load tile map
 tilemap('map0.png',32,32)
